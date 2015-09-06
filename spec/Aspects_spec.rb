bc8ba95c2ac8fe34bbf011e7b2d4e52eb089bdca
santibermejo <santiagobermejo93@gmail.com>
2015-09-06 19:14:23 -0300

    Prueba de push. Arreglo de deprecated y un par de tests

--- spec/Aspects_spec.rb
+++ spec/Aspects_spec.rb
@@ -17,20 +17,30 @@ describe Aspects do
 
   it 'Cuando le paso una clase la agrega a su array de objetos' do
     Aspects.on Aspects do end
-    Aspects.objetos.count.should eq(1)
-    Aspects.objetos.first.should be(Aspects)
+    expect(Aspects.objetos.count).to eq(1)
+    expect(Aspects.objetos.first).to eq(Aspects)
   end
 
   it 'Cuando le paso un regex, agrega su correspondiente clase a su array de objetos' do
     Aspects.on /Aspects/ do end
-    Aspects.objetos.count.should eq(1)
-    Aspects.objetos.first.should be(Aspects)
+    expect(Aspects.objetos.count).to eq(1)
+    expect(Aspects.objetos.first).to eq(Aspects)
   end
 
   it 'Cuando le paso un regex que no matchea con nada, tira ArgumentError' do
     expect {
       Aspects.on /NoMatchea/,/TampocoMatchea/ do end
     }.to raise_error ArgumentError
+  end
+
+  it 'Cuando le paso un regex que no matchea con nada y otro que si, agrega el que si a la lista' do
+      Aspects.on /NoMatchea/, /Aspects/ do end
+      expect(Aspects.objetos.count).to eq(1)
+      expect(Aspects.objetos.first).to eq(Aspects)
+
+      Aspects.on /Aspects/, /NoMatchea/ do end
+      expect(Aspects.objetos.count).to eq(1)
+      expect(Aspects.objetos.first).to eq(Aspects)
   end
 
 end
