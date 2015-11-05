2c05c0e03f42fe27c77f3ddd4acceaf7525c2e11
Diego Anazonian <diego.anazonian@gmail.com>
2015-11-05 20:59:23 -0300

    cambia nombre Santi

---
+++ DragonBallTP/test/tpScala/GuerreroSpec.scala
@@ -0,0 +1,36 @@
+package tpScala
+
+import org.junit.Before
+import org.junit.Assert._
+import org.junit.Test
+import tpScala.Movement.{cargarKi, Movimiento}
+
+class GuerreroSpec {
+  var guerrero: Guerrero = null
+
+  @Before
+  def setUp() = {
+    val items = Array[Item]()
+    val movimientos = Array[Movimiento]()
+    guerrero = new Guerrero("Santi", items, movimientos, 4, 40, Androide, Inconsciente)
+  }
+
+  @Test
+  def unGuerreroAumentaSuKi() = {
+    guerrero = guerrero.aumentarKi(10)
+    assertEquals(14, guerrero.ki)
+  }
+
+  @Test
+  def unAndroideIntentaDescansarParaCargarSuKiPeroNoCargaNada() = {
+    guerrero = guerrero.ejecutarMovimiento(cargarKi)
+    assertEquals(4, guerrero.ki)
+  }
+
+  @Test
+  def unHumanoDescansaParaAumentarSuKiYLoLogra = {
+    /*guerrero = new DGB.Guerrero("Santi", items, movimientos, 4, 40, DGB.Humano, null)
+    assertEquals(4, guerrero.ki)*/
+  }
+
+}
