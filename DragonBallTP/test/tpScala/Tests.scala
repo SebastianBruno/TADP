738e6349a5edf890dc1c00e0f93a0adb8e42f179
Matias Fraboschi <matias.fraboschi@mercadolibre.com.ar>
2015-11-05 23:22:07 -0300

    tests sobre la carga de ki

--- DragonBallTP/test/tpScala/Tests.scala
+++ DragonBallTP/test/tpScala/Tests.scala
@@ -12,7 +12,7 @@ class Tests {
   def setUp() = {
     val items = Array[Item]()
     val movimientos = Array[Movimiento]()
-    guerrero = new Guerrero("Santi", items, movimientos, 4, 40, Androide, Inconsciente)
+    guerrero = new Guerrero("Numero 18", items, movimientos, 4, 40, Androide, Inconsciente)
   }
 
   @Test
@@ -29,8 +29,35 @@ class Tests {
 
   @Test
   def unHumanoDescansaParaAumentarSuKiYLoLogra = {
-    /*guerrero = new DGB.Guerrero("Santi", items, movimientos, 4, 40, DGB.Humano, null)
-    assertEquals(4, guerrero.ki)*/
+    val items = Array[Item]()
+    val movimientos = Array[Movimiento]()
+    guerrero = new Guerrero("Krillin", items, movimientos, 4, 40, Humano, null)
+
+    guerrero = guerrero.ejecutarMovimiento(cargarKi)
+
+    assertEquals(104, guerrero.ki)
+  }
+
+  @Test
+  def unSaiyajinDescansaParaAumentarSuKiYLoLogra = {
+    val items = Array[Item]()
+    val movimientos = Array[Movimiento]()
+    guerrero = new Guerrero("Vegetta", items, movimientos, 4, 40, Saiyajin(cola = true, transformacion = None), null)
+
+    guerrero = guerrero.ejecutarMovimiento(cargarKi)
+
+    assertEquals(104, guerrero.ki)
+  }
+
+  @Test
+  def unSuperSaiyajinDescansaParaAumentarSuKiYLoLogra = {
+    val items = Array[Item]()
+    val movimientos = Array[Movimiento]()
+    guerrero = new Guerrero("Goku", items, movimientos, 4, 40, Saiyajin(cola = true, transformacion = Some(SuperSaiyajin(nivel = 1))), null)
+
+    guerrero = guerrero.ejecutarMovimiento(cargarKi)
+
+    assertEquals(154, guerrero.ki)
   }
 
 }
